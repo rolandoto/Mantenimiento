4756b151daedf9a6472d1b81ca9664568e960fe0
Rolando <rolando22_@outlook.com>
2022-05-17 15:41:52 -0500

    adding script for heroku

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const app = require("./app");
 require("./database");
 
+const port  =process.env.PORT || 4000
+
 async function init() {
-    await app.listen(process.env.PORT || 4000);
+    await app.listen(port);
     console.log("App running on port: ", process.env.PORT);
 }
 
